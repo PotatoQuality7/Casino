25a6c1e37dadef70d1597c1d1853f04cc58ddd06
PotatoQuality7 <bertocrespo7@gmail.com>
2023-10-23 00:45:45 +0200

    Versão 1.23
    Melhorias no Menu

--- Casino/index.js
+++ Casino/index.js
@@ -1,12 +1,23 @@
 import './styles.css';
 import { useState } from 'react';
 import Login from '../Login/index.js';
+import React from 'react';
 
 function Casino() {
 	
 	const [conteudo,setConteudo] = useState("");
 	const [display,setDisplay] = useState("block");
-	
+
+	var atual = 0;
+	var limite = 0;
+	var dir = [];
+	var adjust = [
+		[0,0],
+		[0,0],
+		[0,0],
+		[0,0],
+	];
+
 	const logout = (e) => {
 		setConteudo(<Login />);
 		setDisplay("none");
@@ -16,24 +27,18 @@ function Casino() {
 
 		const [nome,setNome] = useState("Algoo");
 		const [senha,setSenha] = useState("Algoo");
-		const [cor,setCor] = useState("red");
+		const [cor1, setCor1] = useState("yellow");	
+		const [cor2, setCor2] = useState("purple");	
 
 		const [flt, setFlt] = useState([
-			{x: 600, y: 600},
-			{x: 600, y: 600},
-			{x: 600, y: 600},
-			{x: 600, y: 600},
+			{x: -120, y: 500},
+			{x: 520, y: -30},
+			{x: 520, y: 945},
+			{x: 1152, y: 500},
 		])
 
-		const [dir, setDir] = useState([
-			{x: 0, y: 0},
-			{x: 0, y: 0},
-			{x: 0, y: 0},
-			{x: 0, y: 0},
-		]);
-
-		const [duracao, setDuracao] = useState(0);
-		
+		const [duracao, setDuracao] = useState([0,0]);
+			
 		const upNome = (e) => {
 			setNome(e.target.value);
 		}
@@ -50,92 +55,109 @@ function Casino() {
 			setDisplay("none");
 		}
 
+		function coisa() {
+			console.log("Tarde");
+		}		
+
 		const moverButoes = (e) => {
-			if (duracao[0] == duracao[1])
+			if (atual == limite) {
 				gerarDirecoes();
-			const temp = [];		
-			for (let i = 0; i <= 3; i++) {
-				temp.push([
-					{x: (flt[i].x+dir[i].x), y: (flt[i].y+dir[i].y)}
-				]);
 			}
-			alert("ss");
-			//setFlt(temp);
-			//setDuracao([duracao[0],duracao[1]++);
+			setCor1(cor1 == "yellow"? "purple" : "yellow");		
+			setCor2(cor2 == "yellow"? "purple" : "yellow");		
+			for (let i = 0; i <= 3; i++) {
+				adjust[i][0] = 0;
+				adjust[i][1] = 0;
+				switch (flt[i].x) {
+					case -120: adjust[i][0] = 1214; break;
+					case 1152: adjust[i][0] = -1214; break;			
+				}
+				switch (flt[i].y) {
+					case -30: adjust[i][1] = 934; break;
+					case 945: adjust[i][1] = -934; break;			
+				}
+			}
+		
+			setFlt([
+				{x: (flt[0].x+dir[0][0]+adjust[0][0]), y: (flt[0].y+dir[0][1]+adjust[0][1])},
+				{x: (flt[1].x+dir[1][0]+adjust[1][0]), y: (flt[1].y+dir[1][1]+adjust[1][1])},
+				{x: (flt[2].x+dir[2][0]+adjust[2][0]), y: (flt[2].y+dir[2][1]+adjust[2][1])},
+				{x: (flt[3].x+dir[3][0]+adjust[3][0]), y: (flt[3].y+dir[3][1]+adjust[3][1])},
+			]);
+			atual++;
+			//setDuracao([duracao[0],++duracao[1]]);
 		}
 
+		//let algo = setInterval(moverButoes, 2000);
+
 		function gerarDirecoes() {
-			setDuracao([(Math.random() * 15) + 10,0]);
-			setDuracao([ (Math.random() * 15) , 5]);
-			const temp = [];
+			dir = [];
+			let x, y;
 			for (let i = 0; i <= 3; i++) {
-				temp.push([
-					{x: (Math.random()*3)-1, y: (Math.random()*3)-1}
-				]);
+				x = (Math.trunc(Math.random()*3)-1);
+				y = (Math.trunc(Math.random()*3)-1);
+				if (x == 0 && y == 0)
+					i--;
+				 else
+					dir.push([x,y]);
 			}
-			temp.push([
-				{x: (Math.random()*3)-1, y: (Math.random()*3)-1}
-			]);
-
-			setDir([
-				{x: 0, y: 0},
-				{x: 0, y: 0},
-				{x: 0, y: 0},
-				{x: 0, y: 0},
-			]);
-
-			//setDir(temp);
-			alert(dir[3].x);
+			atual = 0;
+			limite = Math.trunc((Math.random() * 51) + 250);
+			//setDuracao([(Math.trunc(Math.random() * 15) + 10),32]);
 		}
 
 		return (
-			<div>
-				<div style={{"display": display}}>
-					<div>
-						<h1>Casino</h1>
-						<button onClick={logout}>POWER</button>
-						<button id="jogo-1" className="jogos">Torre de Blocos</button>
-						<button id="jogo-2" className="jogos">Tetris</button>
-						<button id="jogo-3" className="jogos">SkatePong</button>
-						<button id="jogo-4" className="jogos">Snake</button>
-						<button id="jogo-5" className="jogos">Bounce</button>
-						<button id="jogo-6" className="jogos">F1 Race</button>
-					</div>
-					<nav>
-						<button id="flt-1" className="flutuantes" 
-						ref={el => {
-				            if (el) {
-				              el.style.setProperty('top', (flt[0].x+"px"), 'important');
-				              el.style.setProperty('left', (flt[0].y+"px"), 'important');
-				            }
-				     	 }}>Perfil</button>
-						<button id="flt-2" className="flutuantes" 
-						ref={el => {
-				            if (el) {
-				              el.style.setProperty('top', (flt[1].x+"px"), 'important');
-				              el.style.setProperty('left', (flt[1].y+"px"), 'important');
-				            }
-				     	 }}>Estatisticas</button>
-						<button id="flt-3" className="flutuantes" 
-						ref={el => {
-				            if (el) {
-				              el.style.setProperty('top', (flt[2].x+"px"), 'important');
-				              el.style.setProperty('left', (flt[2].y+"px"), 'important');
-				            }
-				     	 }}>Transferencias</button>
-						<button id="flt-4" className="flutuantes" 
-						ref={el => {
-				            if (el) {
-				              el.style.setProperty('top', (flt[3].x+"px"), 'important');
-				              el.style.setProperty('left', (flt[3].y+"px"), 'important');
-				            }
-				     	 }}>Documentacao</button>						
-					</nav>
-					
-					<button className="jogos" onClick={gerarDirecoes}>Algo</button>
-					<label>{flt[0].y}--{flt[0].x}</label>
+
+			<div id="tela-borda" ref={el => {
+		            if (el) {
+					  let pattern = "repeating-linear-gradient(-45deg, "+cor1+" 9px, "+cor1+", "+cor2+" 18px, "+cor2+", "+cor1+" 27px)";
+		              el.style.setProperty("background", pattern, 'important'); }}}>
+				<div id="tela">
+					<div style={{"display": display}}>
+						<div>
+							<h1>Casino</h1>
+							<button onClick={logout}>POWER</button>
+							<div id="borda-1" className="bordas"><button id="jogo-1" className="jogos">Torre de Blocos</button></div>
+							<div id="borda-2" className="bordas"><button id="jogo-2" className="jogos">Tetris</button></div>
+							<div id="borda-3" className="bordas"><button id="jogo-3" className="jogos">SkatePong</button></div>
+							<div id="borda-4" className="bordas"><button id="jogo-4" className="jogos">Snake</button></div>
+							<div id="borda-5" className="bordas"><button id="jogo-5" className="jogos">Bounce</button></div>
+							<div id="borda-6" className="bordas"><button id="jogo-6" className="jogos">F1 Race</button></div>
+						</div>
+						<nav>
+							<button id="flt-1" className="flutuantes" 
+							ref={el => {
+					            if (el) {
+					              el.style.setProperty('top', (flt[0].y+"px"), 'important');
+					              el.style.setProperty('left', (flt[0].x+"px"), 'important');
+					            }
+					     	 }}>Perfil</button>
+							<button id="flt-2" className="flutuantes" 
+							ref={el => {
+					            if (el) {
+					              el.style.setProperty('top', (flt[1].y+"px"), 'important');
+					              el.style.setProperty('left', (flt[1].x+"px"), 'important');
+					            }
+					     	 }}>Estatisticas</button>
+							<button id="flt-3" className="flutuantes" 
+							ref={el => {
+					            if (el) {
+					              el.style.setProperty('top', (flt[2].y+"px"), 'important');
+					              el.style.setProperty('left', (flt[2].x+"px"), 'important');
+					            }
+					     	 }}>Transferencias</button>
+							<button id="flt-4" className="flutuantes" 
+							ref={el => {
+					            if (el) {
+					              el.style.setProperty('top', (flt[3].y+"px"), 'important');
+					              el.style.setProperty('left', (flt[3].x+"px"), 'important');
+					            }
+					     	 }}>Documentacao</button>						
+						</nav>
+						<button onClick={moverButoes}>Algo</button>
 					</div>
 					{conteudo}
+				</div>
 			</div>
 		)		
 	}
@@ -143,9 +165,13 @@ function Casino() {
 	return (
 		<div>
 			{/*retirar esse useState*/}
-			<div id="bordas" style={{"display": display}}>
+			<div style={{"display": display}}>
 				<title>Tela do Casino</title>
 				<Menu />
+				{/*<div>
+					<div id="cortina-l"></div>
+					<div id="cortina-r"></div>
+				</div> */}
 			</div>
 			{conteudo}
 		</div>
---
+++ Casino/script.js
@@ -0,0 +1,9 @@
+let algo = setInterval(cobaia, 5000);
+let cobaia = document.getElementById("cobaia");
+
+function batata() {
+	alert("Script file");
+	cobaia.style.left = "500px";
+	cobaia.style.top = "800px";
+	cobaia.style.background = "red";
+}
